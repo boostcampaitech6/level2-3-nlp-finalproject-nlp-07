5e06eced2697a18996dfeee0172939145ec69b64
devBuzz142 <devbuzz142@gmail.com>
2024-03-13 17:38:32 +0900

    feat: 미디 받아서 트랙 추가해주기

--- server/api.py
+++ server/api.py
@@ -7,14 +7,17 @@ from miditok import MMM, TokenizerConfig
 import torch
 import os, shutil
 from symusic import Score
+from pathlib import Path
 import logging
 # 로깅 설정
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 from pydantic import BaseModel
-from server.utils.generateModel_util import generate_initial_track, generate_additional_track
+from utils.generateModel_util import generate_initial_track, generate_additional_track
 from transformers import AutoTokenizer
-from server.utils.frontModel_util import customRobertaForSequenceClassification, id2labelData_labels
+from utils.frontModel_util import customRobertaForSequenceClassification, id2labelData_labels
+from utils.tokenizer_converter import mmm_to_nnn, nnn_to_mmm
+from tokenizer_svr import get_nnn_tokenizer
 
 class TextData(BaseModel):
     prompt: str
@@ -22,25 +25,25 @@ class TextData(BaseModel):
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-MODEL_DIR = "./model/"
+MODEL_DIR = "../ml/models"
 TEMP_DIR = "./temp/"
 
-MODEL_NAME = "bar4-ch4-checkpoint-8100"
+MODEL_NAME = "nnn-vel8-lakh-checkpoint-16800"
 
 model_path = os.path.join(MODEL_DIR, MODEL_NAME)
 model = GPT2LMHeadModel.from_pretrained(model_path) 
 
-tokenizer_path = os.path.join(MODEL_DIR, MODEL_NAME+'/tokenizer.json')
-tokenizer = MMM(TokenizerConfig(), tokenizer_path)
+# tokenizer_path = os.path.join(MODEL_DIR, MODEL_NAME+'/tokenizer.json')
+tokenizer = get_nnn_tokenizer(8)
 
 # 모델 필요
-front_model_path = 'SangGank/my-front-model'
-front_model = customRobertaForSequenceClassification.from_pretrained(front_model_path)
-front_tokenizer = AutoTokenizer.from_pretrained(front_model_path)
+# front_model_path = 'SangGank/my-front-model'
+# front_model = customRobertaForSequenceClassification.from_pretrained(front_model_path)
+# front_tokenizer = AutoTokenizer.from_pretrained(front_model_path)
 
-# pickle 저장 위치
-pickle_path = './model/labels.pkl'
-emotion_dict , tempo_dict, genre_dict = id2labelData_labels(pickle_path)
+# # pickle 저장 위치
+# pickle_path = './model/labels.pkl'
+# emotion_dict , tempo_dict, genre_dict = id2labelData_labels(pickle_path)
 
 @router.post("/generate_midi/")
 async def generate_midi(req: TextData):
@@ -63,22 +66,23 @@ async def generate_midi(req: TextData):
     text = req.prompt
     logging.info(f"input_text : {text}")
 
-    inputs = front_tokenizer(text, return_tensors='pt')
-    result = front_model(**inputs).logits
+    # inputs = front_tokenizer(text, return_tensors='pt')
+    # result = front_model(**inputs).logits
 
-    emotion_id = int(result[0].detach().argmax())
-    tempo_id = int(result[1].detach().argmax())
-    genre_id = int(result[2].detach().argmax())
+    # emotion_id = int(result[0].detach().argmax())
+    # tempo_id = int(result[1].detach().argmax())
+    # genre_id = int(result[2].detach().argmax())
 
-    emotion , tempo, genre = emotion_dict[emotion_id], tempo_dict[tempo_id], genre_dict[genre_id]
+    # emotion , tempo, genre = emotion_dict[emotion_id], tempo_dict[tempo_id], genre_dict[genre_id]
     
-    # 로그에 텍스트 출력
-    logging.info("emotion : %s,  tempo : %s,  genre : %s", emotion, tempo, genre)
+    # # 로그에 텍스트 출력
+    # logging.info("emotion : %s,  tempo : %s,  genre : %s", emotion, tempo, genre)
     
     ## generation midi
     generated_ids = generate_initial_track(model, tokenizer, temperature=0.8)
 
-    midi_data = tokenizer.tokens_to_midi(generated_ids[0])
+    mmm_tokens_ids = nnn_to_mmm(generated_ids[0].tolist(), tokenizer)
+    midi_data = tokenizer.tokens_to_midi(mmm_tokens_ids)
 
     file_path = os.path.join(TEMP_DIR, "temp_gen.mid")
     midi_data.dump_midi(file_path)
@@ -87,18 +91,27 @@ async def generate_midi(req: TextData):
     return StreamingResponse(open(file_path, "rb"), media_type="audio/midi")
 
 @router.post("/upload_midi/")
-# async def receive_midi(midi_file: UploadFile = File(...), instnum: int = Form(...)) -> str:
 async def receive_midi(midi_file: UploadFile = File(...), instnum: int = Form(...)):
     logging.info(f"midi_file : {midi_file}")
     logging.info(f"instnum : {instnum}")
 
-    temp_file_path = os.path.join(TEMP_DIR, "temp_upload.mid")
+    temp_file_path = os.path.join(TEMP_DIR, "temp_receive.mid")
     try:
         # 업로드된 파일을 임시 폴더에 저장
         with open(temp_file_path, "wb") as temp_file:
             shutil.copyfileobj(midi_file.file, temp_file)
-        
-        # 임시 파일 경로 반환
-        return {"status": "success", "temp_file_path": temp_file_path}
+        midi = Score(temp_file_path)
     except Exception as e:
         return {"status": "failed", "message": str(e)}
+    
+    mmm_tokens_ids = tokenizer(midi).ids
+    nnn_tokens_ids = mmm_to_nnn(mmm_tokens_ids, tokenizer)
+    nnn_tokens_ids = torch.tensor([nnn_tokens_ids])
+    
+    generated_ids = generate_additional_track(nnn_tokens_ids, model, tokenizer, temperature=0.8)
+    mmm_generated_ids = nnn_to_mmm(generated_ids[0].tolist(), tokenizer)
+    midi_data = tokenizer.tokens_to_midi(mmm_generated_ids)
+    file_path = os.path.join(TEMP_DIR, "temp_additional.mid")
+    midi_data.dump_midi(file_path)
+    
+    return FileResponse(file_path, media_type="audio/midi")
---
+++ server/tokenizer_svr.py
@@ -0,0 +1,107 @@
+from miditok import MMM, TokenizerConfig
+from typing import Union, Optional
+from pathlib import Path
+
+class CodeplayTokenizer(MMM):
+    def __init__(self, config: TokenizerConfig):
+        super().__init__(config)
+    
+    def save_pretrained(
+        self,
+        save_directory: Union[str, Path],
+        *,
+        config: Optional[Union[dict, "DataclassInstance"]] = None,
+        repo_id: Optional[str] = None,
+        push_to_hub: bool = False,
+        **push_to_hub_kwargs,
+    ) -> Optional[str]:
+        """
+        Save weights in local directory.
+
+        Args:
+            save_directory (`str` or `Path`):
+                Path to directory in which the model weights and configuration will be saved.
+            config (`dict` or `DataclassInstance`, *optional*):
+                Model configuration specified as a key/value dictionary or a dataclass instance.
+            push_to_hub (`bool`, *optional*, defaults to `False`):
+                Whether or not to push your model to the Huggingface Hub after saving it.
+            repo_id (`str`, *optional*):
+                ID of your repository on the Hub. Used only if `push_to_hub=True`. Will default to the folder name if
+                not provided.
+            kwargs:
+                Additional key word arguments passed along to the [`~ModelHubMixin.push_to_hub`] method.
+        """
+        save_directory = Path(save_directory)
+        save_directory.mkdir(parents=True, exist_ok=True)
+
+        # save model weights/files (framework-specific)
+        self._save_pretrained(save_directory)
+
+        # push to the Hub if required
+        if push_to_hub:
+            kwargs = push_to_hub_kwargs.copy()  # soft-copy to avoid mutating input
+            if config is not None:  # kwarg for `push_to_hub`
+                kwargs["config"] = config
+            if repo_id is None:
+                repo_id = save_directory.name  # Defaults to `save_directory` name
+            return self.push_to_hub(repo_id=repo_id, **kwargs)
+        return None
+
+GENRE_TOKEN_LIST = ['Rock', 'Pop', 'Jazz']
+GENRE_TOKEN_LIST = ['Genre_Unk'] + ['Genre_'+genre for genre in GENRE_TOKEN_LIST]
+GENRE_TOKEN_LIST += ['Genre_'+str(i+1) for i in range(40-len(GENRE_TOKEN_LIST))] #40
+BAR2_TOKEN_LIST = ['Bar2_Unk'] + ['Bar2_'+str(i+1) for i in range(127)] # 128
+
+def get_custom_tokenizer():
+    TOKENIZER_NAME = CodeplayTokenizer
+    config = TokenizerConfig(
+        num_velocities=16,
+        use_chord=True,
+        use_pitch_intervals=True,
+        use_programs=True,)
+    tokenizer = TOKENIZER_NAME(config)
+    
+    # MMM tokenizer
+    mmm = len(tokenizer)-1
+    print(f'MMM Tokenizer bandwith : 0 ~ {mmm}, ({mmm+1} tokens)')
+    
+    # Add genre token
+    for genre_tk in GENRE_TOKEN_LIST:
+        tokenizer.add_to_vocab(genre_tk)
+    genre = len(tokenizer)-1
+    print(f'Genre Tokenizer bandwith : {mmm+1} ~ {genre}, ({genre-mmm} tokens)')
+    
+    # Add cut(bar4) token
+    for cut_tk in BAR2_TOKEN_LIST:
+        tokenizer.add_to_vocab(cut_tk)
+    # Add cut Unused token
+    cut = len(tokenizer)-1
+    print(f'Bar2 Cut Tokenizer bandwith : {genre+1} ~ {cut}, ({cut-genre} tokens)')
+    
+    print(f'Total Tokenizer bandwith : 0 ~ {cut}, ({len(tokenizer)} tokens)')
+    return tokenizer
+
+def get_nnn_tokenizer(num_velocities=8):
+    NNN = CodeplayTokenizer
+    config = TokenizerConfig(
+        num_velocities=num_velocities,
+        use_programs=True
+    )
+    tokenizer = NNN(config)
+    prev_len = len(tokenizer)
+    vocabs = list(tokenizer.vocab.keys())
+    
+    pitches = [v for v in vocabs if v.startswith('Pitch_') ]
+    velocities = [v for v in vocabs if v.startswith('Velocity_') ]
+    durations = [v for v in vocabs if v.startswith('Duration_') ]
+    
+    for p in pitches:
+        for v in velocities:
+            for d in durations:
+                new_tk = f'{p}+{v}+{d}'
+                tokenizer.add_to_vocab(new_tk)
+    
+    print(f'MMM Tokenizer bandwith : 0 ~ {prev_len}, ({prev_len} tokens)')
+    print(f'NNN Tokenizer bandwith : {prev_len} ~ {len(tokenizer)}, ({len(tokenizer)-prev_len} tokens)')
+    return tokenizer
+    
