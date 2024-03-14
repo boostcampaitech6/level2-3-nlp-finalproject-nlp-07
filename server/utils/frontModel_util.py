faeff3c04cb41e92d4dec59ad7938da3e7cc82f2
seung <gnues71@naver.com>
2024-03-14 11:38:30 +0900

    feat: bert 기반 front model 추가

--- server/utils/frontModel_util.py
+++ server/utils/frontModel_util.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from transformers import BertPreTrainedModel, RobertaModel, AutoTokenizer
+from transformers import BertPreTrainedModel, RobertaModel, BertModel, AutoTokenizer
 from transformers.modeling_outputs import SequenceClassifierOutput
 from typing import Optional, Tuple, Union
 
@@ -8,6 +8,85 @@ from tqdm import tqdm
 import pickle
 import os
 from settings import MODEL_DIR, FRONT_MODEL_NAME, PICKLE_PATH
+
+
+class customBertForSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config, num_labels1 = None, num_labels2 = None, num_labels3 = None ):
+        super().__init__(config)
+        self.num_labels1 = config.num_labels1
+        self.num_labels2 = config.num_labels2
+        self.num_labels3 = config.num_labels3
+        self.config = config
+
+        self.bert = BertModel(config)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.classifier1 = nn.Linear(config.hidden_size, self.num_labels1)
+        self.classifier2 = nn.Linear(config.hidden_size, self.num_labels2)
+        self.classifier3 = nn.Linear(config.hidden_size, self.num_labels3)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels1: Optional[torch.Tensor] = None,
+        labels2: Optional[torch.Tensor] = None,
+        labels3: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.Tensor], SequenceClassifierOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        logits1 = self.classifier1(pooled_output)
+        logits2 = self.classifier2(pooled_output)
+        logits3 = self.classifier3(pooled_output)
+
+        loss1 = None
+        loss2 = None
+        loss3 = None
+        
+        loss =None
+        if loss1 and loss2 and loss3:
+            loss = loss1 + loss2 + loss3
+        if not return_dict:
+            output = (logits1,) + (logits2,) + (logits3,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=(logits1,) + (logits2,) + (logits3,),
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
 class customRobertaForSequenceClassification(BertPreTrainedModel):
     def __init__(self, config, num_labels1 = None, num_labels2 = None, num_labels3 = None ):
