20955a820238cd5c94500245d2805897b117acc5
seung <gnues71@naver.com>
2024-03-20 17:00:34 +0900

    feat: midi temp function

--- server/utils/utils.py
+++ server/utils/utils.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import mido
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -14,3 +15,44 @@ def clear_huggingface_cache(delete: bool):
             logging.info(f"Error clearing Hugging Face cache: {e}")
     else:
         logging.info("Hugging Face cache directory not found.")
+
+def extract_tempo(midi_file_path):
+    try:
+        mid = mido.MidiFile(midi_file_path)
+        
+        # Tempo 정보가 있는 첫 번째 트랙을 찾음
+        for track in mid.tracks:
+            for msg in track:
+                if msg.type == 'set_tempo':
+                    # Tempo 정보를 microseconds per beat 단위로 변환
+                    tempo = mido.tempo2bpm(msg.tempo)
+                    return tempo
+        # 템포 정보가 없는 경우
+        print("No tempo information found in MIDI file.")
+        return None
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+
+def modify_tempo(midi_file_path, new_tempo_bpm):
+    try:
+        mid = mido.MidiFile(midi_file_path)
+        ticks_per_beat = mid.ticks_per_beat
+        
+        # 새로운 템포 값으로 변환
+        new_tempo = mido.bpm2tempo(new_tempo_bpm)
+        
+        # 템포 메시지 생성
+        tempo_msg = mido.MetaMessage('set_tempo', tempo=new_tempo, time=0)
+        
+        # 첫 번째 트랙의 첫 이벤트로 삽입
+        mid.tracks[0].insert(0, tempo_msg)
+        
+        # 새로운 파일 저장
+        mid.save(midi_file_path)
+        
+        print(f"Tempo modified successfully. New tempo: {new_tempo_bpm} BPM")
+    except Exception as e:
+        print(f"Error: {e}")
