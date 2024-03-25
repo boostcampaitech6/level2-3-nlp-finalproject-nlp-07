aecdb1914815ea24e275a0ee3675f0546339bdd4
devbuzz142 <devbuzz142@gmail.com>
2024-03-25 18:26:39 +0900

    bug fix

--- ml/src/train.py
+++ ml/src/train.py
@@ -109,7 +109,7 @@ def main(args):
     
     # Get the output directory with timestamp.
     OUTPUT_DIR = os.path.join(BASE_DIR, f"..{args.save_path}")
-    save_path = OUTPUT_DIR + f"/cp02-finetune-{args.dataset}-{args.tokenizer}/{datetime.now().strftime('%Y%m%d%H%M%S')}"
+    save_path = OUTPUT_DIR + f"/{args.dataset}-{args.tokenizer}/{datetime.now().strftime('%Y%m%d%H%M%S')}"
         
     # Commented parameters correspond to the small model
     trainer_config = {
