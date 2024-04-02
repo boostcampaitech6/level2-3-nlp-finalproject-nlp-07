d2350c704fe9a1576a5d73a5cca9b91ff79c1bf6
patcasso <patcasso21@gmail.com>
2024-04-02 12:16:20 +0900

    feat: added tutorial image to tutorial modal

--- frontend/src/components/TutorialModal.react.js
+++ frontend/src/components/TutorialModal.react.js
@@ -4,6 +4,7 @@ import Modal from 'react-bootstrap/Modal';
 import { Container } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
 const TutorialModal = (props) => {
@@ -12,16 +13,34 @@ const TutorialModal = (props) => {
         <Modal
             show={props.showTutorialModal}
             onHide={() => props.setShowTutorialModal(false)}
-            dialogClassName="error-modal"
+            dialogClassName="tutorial-modal"
         >
             <Modal.Header closeButton>
                 <Modal.Title>Tutorial</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Container>
-                    <Row>
-                        <span>Tutorial Content Coming Soon...</span>
-                    </Row>
+                <Container className='m-0 p-0'>
+                    <Card style={{ border: "2px solid #618eff" }}>
+                        <Card.Header as="h3" style={{ color: "navy", backgroundColor: "#bdcfff" }}>
+                            ① Generate Music
+                        </Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <img src="./misc_img/tutorial1.png" width="1500px" style={{ objectFit: "cover", objectPosition: "center top", height: "220px" }} />
+                                {/* <img src="./misc_img/tutorial1.png" width="1500px" /> */}
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                    <Card className='mt-3' style={{ border: "2px solid #618eff" }}>
+                        <Card.Header as="h3" style={{ color: "navy", backgroundColor: "#bdcfff" }}>
+                            ② Edit Music
+                        </Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <img src="./misc_img/tutorial2.png" width="1500px" />
+                            </Row>
+                        </Card.Body>
+                    </Card>
                     <Row className="mt-4 float-end">
                         <Col>
                             <Button
